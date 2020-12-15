36f5cc17545dc8bff273ce94c825598b3157ddbd
Janio Isacura <janioisacura@gmail.com>
2020-12-15 16:46:54 -0300

    Validación de inputs a través de componente

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+
+  constructor( private fb:FormBuilder) {
+    this.crearFormulario();
+  }
 
   ngOnInit(): void {
+  }
+
+  crearFormulario(){
+    this.forma = this.fb.group({
+      //1 arg = value, 2 = validador síncrono
+      nombre:   ['', [ Validators.required, Validators.minLength(3) ]],
+      apellido: ['', [ Validators.required, Validators.minLength(3) ]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ]]
+    });
+   //     nombre:   ['Sheev', [ Validators.required, Validators.minLength(3) ]],
+   //     apellido: ['Palpatine', [ Validators.required, Validators.minLength(3) ]],
+   //     email:    ['iamthesenate@sith.com', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ]]
+   //   });
+  }
+
+  guardar(){
+    console.log(this.forma);
   }
 
 }
